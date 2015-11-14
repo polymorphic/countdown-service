8d44a72bd41135a5655bad0671d9e0b98d473fa6
Dragos Manolescu <coder@micro-workflow.com>
2015-11-14 09:59:24 -0800

    Added Procfile

--- src/main/scala/com/microworkflow/Boot.scala
+++ src/main/scala/com/microworkflow/Boot.scala
@@ -12,6 +12,8 @@ object Boot extends App {
 
   val port = sys.env.get("PORT").map(_.toInt).getOrElse(8080)
 
+  actorSystem.log.info(s"Binding on port $port")
+
   IO(Http) ! Http.Bind(router, interface = "0.0.0.0", port = port)
 
 }
