649ee91f02e9a31549e6c29f8930ce6b6e2710e6
Dragos Manolescu <coder@micro-workflow.com>
2016-11-05 18:50:27 -0700

    Added canned response

--- src/main/scala/com/microworkflow/RestRouter.scala
+++ src/main/scala/com/microworkflow/RestRouter.scala
@@ -1,10 +1,13 @@
 package com.microworkflow
 
 import akka.actor.{Actor, ActorLogging, Props}
+import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.microworkflow.SkillHandler.HandleSkill
+import spray.http.MediaTypes
 import spray.http.StatusCodes._
+import spray.httpx.marshalling.Marshaller
 import spray.routing._
 import spray.util.LoggingContext
 
@@ -21,6 +24,7 @@ object RestRouter {
     </body>
   </html>
 }
+
 
 class RestRouter extends Actor with HttpService with ActorLogging {
 
@@ -97,9 +101,15 @@ class SkillHandler(rc: RequestContext) extends Actor with ActorLogging {
   val mapper = new ObjectMapper()
   mapper.registerModule(DefaultScalaModule)
 
+  val objectNodeMarshaller =
+    Marshaller.delegate[ObjectNode, String](MediaTypes.`application/json`)(objectNode ⇒
+      mapper.writeValueAsString(objectNode)
+  )
+
   override def receive: Actor.Receive = {
     case HandleSkill(jsonNode) ⇒
-      rc.complete("ok")
+      val requestType = Try { jsonNode.get("request").get("type").asText() }
+      rc.complete(SkillHandler.buildResponse())(objectNodeMarshaller)
   }
 }
 
@@ -108,4 +118,23 @@ object SkillHandler {
 
   sealed trait SkillHandlerProtocol
   case class HandleSkill(jsonNode: JsonNode) extends SkillHandlerProtocol
+
+  val mapper = new ObjectMapper()
+  mapper.registerModule(DefaultScalaModule)
+
+  /*
+  https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/alexa-skills-kit-interface-reference#response-format
+   */
+  def buildResponse(): ObjectNode = {
+    val r = mapper.createObjectNode()
+    r.put("version", "1.0")
+    val responseElement = mapper.createObjectNode()
+    responseElement.put("shouldEndSession", true)
+    val outputSpeechElement = mapper.createObjectNode()
+    outputSpeechElement.put("type", "PlainText")
+    outputSpeechElement.put("text", "Today will provide you a new learning opportunity. Stick with it and the possibilities will be endless.")
+    responseElement.set("outputSpeech", outputSpeechElement)
+    r.set("response", responseElement)
+    r
+  }
 }
